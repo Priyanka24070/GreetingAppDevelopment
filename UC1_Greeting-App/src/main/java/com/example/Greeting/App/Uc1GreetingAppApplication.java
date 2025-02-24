2bbfc3e7aed7b0aaf28babaf3a26241c5b82af48
Priyanka. <priyankakumarixy0070@gmail.com>
2025-02-24 21:59:43 +0530

    [Priyanka] UC1 Greeting controller

---
+++ UC1_Greeting-App/src/main/java/com/example/Greeting/App/Uc1GreetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Greeting.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc1GreetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc1GreetingAppApplication.class, args);
+	}
+
+}
